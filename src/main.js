64052cb427db4b9910760c12e6dc30545dc32960
LiuJing <2583695839@qq.com>
2020-08-19 16:09:15 +0800

    手动按需引入vant-ui组件

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Button from 'vant/lib/button';
 
+import 'vant/lib/button/style';
+
+Vue.use(Button);
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
