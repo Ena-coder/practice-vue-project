8f9a76323d033ce745f1bd1530d858951f38b51a
LiuJing <2583695839@qq.com>
2020-08-19 17:07:59 +0800

    解决因新建about页面的样式中lang=less导致的报错

---
+++ src/components/About.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="about">
+    <h2>about</h2>
+  </div>
+</template>
+<script>
+export default {
+  name:"About",
+  components:{
+
+  },
+  props:{
+
+  },
+  vuex:{
+
+  },
+  data(){
+  return{}
+  },
+  computed:{
+
+  },
+  watch:{
+
+  },
+  methods:{
+
+  },
+  beforeCreate(){
+
+  },
+  created(){
+
+  },
+  beforeMount(){
+
+  },
+  mounted(){
+
+  }
+}
+</script>
+<style scoped>
+
+</style>
